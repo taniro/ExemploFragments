d3a7577093e1d611bbe2fdfe9c55e607f93b0b0e
taniro <tanirocr@gmail.com>
2021-03-25 22:22:50 -0300

    update 2021

--- app/src/main/java/tads/eaj/ufrn/exemplofragments/MainActivity.kt
+++ app/src/main/java/tads/eaj/ufrn/exemplofragments/MainActivity.kt
@@ -2,9 +2,13 @@ package tads.eaj.ufrn.exemplofragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var button1:Button
+    lateinit var button2:Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,10 +48,35 @@ class MainActivity : AppCompatActivity() {
         // Replace whatever is in the fragment_container view with this fragment,
         // and add the transaction to the back stack
         ft2.replace(R.id.layout_alvo, newFragment)
-        //ft2.addToBackStack(null)
+        ft2.addToBackStack(null)
         // Commit the transaction
         ft2.commit()
+        */
 
+        /*
+
+        button1 = findViewById(R.id.btn1)
+        button2 = findViewById(R.id.btn2)
+
+        button1.setOnClickListener {
+            var fm = supportFragmentManager
+            var fragmentTransaction = fm.beginTransaction()
+            var f1 = Fragment1()
+            fragmentTransaction.replace(R.id.layout_alvo, f1, "Fragment 1")
+            fragmentTransaction.addToBackStack(null)
+            fragmentTransaction.commit()
+        }
+
+        button2.setOnClickListener {
+            var fm = supportFragmentManager
+            var fragmentTransaction = fm.beginTransaction()
+            var f2 = Fragment2()
+            fragmentTransaction.replace(R.id.layout_alvo, f2, "Fragment 2")
+            fragmentTransaction.addToBackStack(null)
+            fragmentTransaction.commit()
+        }
          */
+
+
     }
 }
